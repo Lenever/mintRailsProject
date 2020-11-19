d9b736fcb8198b0c6adab947d9c9616acdf41781
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-11-19 13:55:03 +0100

    Display static data

--- mintRailsProject/mintRailsProject/ListCommitsViewController.swift
+++ mintRailsProject/mintRailsProject/ListCommitsViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class ListCommitsViewController: UITableViewController {
+    var commits = ["zero","one","two","three","four","five"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count 
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) //as? ListViewCell
+        let path = commits[indexPath.row]
+        
+        cell.textLabel?.text = path
+        
+        return cell 
     }
 }
 
