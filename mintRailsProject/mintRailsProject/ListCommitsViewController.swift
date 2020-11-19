7ad636bd5969eb6fd99a7eb913cf43f6111c90a2
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-11-19 12:59:32 +0100

    delete storyboard and launch app from SceneDelegate

--- mintRailsProject/mintRailsProject/ListCommitsViewController.swift
+++ mintRailsProject/mintRailsProject/ListCommitsViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ListCommitsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
 
