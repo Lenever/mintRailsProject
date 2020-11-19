2aa61552a5d37e3716955def842e89196b02adfd
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-11-19 13:05:14 +0100

    Add SnapKit

---
+++ mintRailsProject/Pods/Target Support Files/Pods-mintRailsProject-mintRailsProjectUITests/Pods-mintRailsProject-mintRailsProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mintRailsProject_mintRailsProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_mintRailsProject_mintRailsProjectUITests
+@end
