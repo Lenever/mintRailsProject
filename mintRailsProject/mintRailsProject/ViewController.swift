b0170c38c54517b29fe8912f13eccbb58d602655
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-11-19 12:52:14 +0100

    Initialize project

---
+++ mintRailsProject/mintRailsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mintRailsProject
+//
+//  Created by Ikechukwu Onuorah on 19/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
